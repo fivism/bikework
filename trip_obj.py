5ed39be890b2548bfeead323dffc01ab3b4effd0
fivism <michael.fivis@gmail.com>
2018-06-18 15:35:23 +0200

    tidying up object before modeling

--- trip_obj.py
+++ trip_obj.py
@@ -1,23 +1,35 @@
-class Trip_obj(object):
-    start_time  = 0     # null initialized time value?
-    end_time    = 0     # 
-    start_st    = 0     # start station ID
-    end_st      = 0     # end station ID
-    total_time  = 0
-    percent_done = 0.0
-   
+import dateutil.relativedelta as tdelta
+from datetime import datetime, timezone
 
-    # we check by minute and initialize any that started this minute 
+
+class Trip_obj(object):
+    # consider keyID for these objects
+    start_time = 0     # null initialized time value?
+    end_time = 0     #
+    start_st = 0     # start station ID
+    end_st = 0     # end station ID
+    minutes_total = 0     # time delta between start and end time in mins
+    minutes_done = 0    # total minutes elapsed
+    percent_done = 0.0   # current percentage of trip complete (for drawing)
+    # this will be zero at creation time
+
+    # we check by minute and initialize any that started this minute
     def __init__(self, start_time, end_time, start_st, end_st):
         self.start_time = start_time
-        self.end_time   = end_time
-        self.start_st   = start_st
-        self.end_st     = end st
-    
-    # we check who is alive and accessible in the alive list
-    # and update accordingly
-    def __update__(self):
-        # TODO pull in new time tuple from outside or feed in?
-        percent_done = new_percent_done
-        
-         
+        self.end_time = end_time
+        self.start_st = start_st
+        self.end_st = end_st
+        self.minutes_total = tdelta.relativedelta(end_time, start_time)
+
+    def __update__():
+        """
+        Ticks the progress of the trip forward one minute
+        """
+        minutes_done += 1
+        percent_done = minutes_done / float(minutes_total)
+
+    def __str__():
+        """
+        toString available for debugging
+        """
+        print()

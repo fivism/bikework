c97e5cd6f1b5d35c15c9c692425ef15a8c272195
fivism <michael.fivis@gmail.com>
2018-06-19 19:15:15 +0200

    dict of stations workaround

--- controller.py
+++ controller.py
@@ -10,8 +10,9 @@ from matplotlib.patches import Polygon
 from matplotlib.collections import PatchCollection
 from matplotlib.colors import Normalize
 #import trip_obj
-import pandas
+import pandas as pd
 import sys
+import csv
 
 # if len(sys.argv) < 3:
 #     sys.exit('Usage: %s trip_csv_file output' % sys.argv[0])
@@ -26,16 +27,59 @@ fig, ax = plt.subplots(figsize=(10, 20))
 # center 59.922056, 10.736092
 # for now using DublinCore bounding box of Oslo with limits:
 # westlimit=10.649384; southlimit=59.886967; eastlimit=10.818299; northlimit=59.955795
-m = Basemap(resolution='i',
-            projection='merc',
-            lat_0=59.922, lon_0=10.736,
-            llcrnrlon=10.65, llcrnrlat=59.887, urcrnrlon=10.8183, urcrnrlat=59.9558)
-m.drawmapboundary(fill_color='#46bcec')
-m.fillcontinents(color='#f2f2f2')
-m.drawcoastlines()
-plt.title('oslo med veldig få polygon')
 
-plt.show()
+
+def plot_base():
+    m = Basemap(resolution='l',
+                projection='merc',
+                lat_0=59.922, lon_0=10.736,
+                llcrnrlon=10.65, llcrnrlat=59.887, urcrnrlon=10.8183, urcrnrlat=59.9558)
+    m.drawmapboundary(fill_color='#46bcec')
+    m.fillcontinents(color='#f2f2f2', lake_color='#46bcec')
+    m.drawcoastlines()
+    plt.title('oslo med veldig få polygon')
+
+
+def read_stations(filename):
+    """
+    This should eventually call stationExtractor for newest list
+    Returns dict of station IDs linked to relevant data bits so that 
+    we skip string matching
+    """
+    with open(filename, mode='r') as infile:
+        reader = csv.reader(infile)
+        out_dict = {rows[0]: rows[1:6] for rows in reader}
+        return out_dict
+    # station_dict = pd.read_csv(filename)
+
+
+def plot_stations(filename):
+    """
+    Plots stations on the basemap, sized according to # of locks
+    """
+
+    return None
+
+
+def plot_area(pos):
+    count = new_areas.loc[new_areas.pos == pos]['count']
+    x, y = m(pos[1], pos[0])
+    size = (count/1000) ** 2 + 3
+    m.plot(x, y, 'o', markersize=size, color='#cccccc', alpha=0.8)
+
+    new_areas.pos.apply(plot_area)
+
+
+station_dict = read_stations("test.csv")
+# station_dict = pd.read_csv("test.csv")
+
+# Demo dict TODO
+print(station_dict['158'][2])
+
+
+# plot_stations()
+# plot_base()
+# plt.show()
 # def generate_objs(trips_data):
 #     """
 #     Given a trips dataframe, generate minute by minute renders

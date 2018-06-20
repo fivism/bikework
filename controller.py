2461566654abcec18af9916e112e68f69e09d4ab
fivism <michael.fivis@gmail.com>
2018-06-20 10:21:12 +0200

    working plotting

--- controller.py
+++ controller.py
@@ -29,57 +29,63 @@ fig, ax = plt.subplots(figsize=(10, 20))
 # westlimit=10.649384; southlimit=59.886967; eastlimit=10.818299; northlimit=59.955795
 
 
-def plot_base():
-    m = Basemap(resolution='l',
-                projection='merc',
-                lat_0=59.922, lon_0=10.736,
-                llcrnrlon=10.65, llcrnrlat=59.887, urcrnrlon=10.8183, urcrnrlat=59.9558)
+def plot_base(m):
     m.drawmapboundary(fill_color='#46bcec')
     m.fillcontinents(color='#f2f2f2', lake_color='#46bcec')
     m.drawcoastlines()
-    plt.title('oslo med veldig få polygon')
+    plt.title('bysykkel flow')
 
 
 def read_stations(filename):
     """
     This should eventually call stationExtractor for newest list
     Returns dict of station IDs linked to relevant data bits so that 
-    we skip string matching
+    we skip row matching
     """
     with open(filename, mode='r') as infile:
-        reader = csv.reader(infile)
+        infile_noheader = infile.readlines()[1:]
+        reader = csv.reader(infile_noheader)
         out_dict = {rows[0]: rows[1:6] for rows in reader}
         return out_dict
     # station_dict = pd.read_csv(filename)
 
 
-def plot_stations(filename):
+def plot_stations(sta_dict, m):
     """
-    Plots stations on the basemap, sized according to # of locks
+    Plots stations on basemap given dict which includes lat/longs
+    (lat: dict[4], long: dict[5])
     """
-
-    return None
-
-
-def plot_area(pos):
-    count = new_areas.loc[new_areas.pos == pos]['count']
-    x, y = m(pos[1], pos[0])
-    size = (count/1000) ** 2 + 3
-    m.plot(x, y, 'o', markersize=size, color='#cccccc', alpha=0.8)
-
-    new_areas.pos.apply(plot_area)
+    for st in sta_dict:
+        x, y = float(sta_dict[st][4]), float(sta_dict[st][3])
+        size = (float(sta_dict[st][2]) / 12.0) ** 2
+        m.plot(x, y, marker='o',
+               markersize=size, color='#cccccc', alpha=0.9, latlon=True)
 
 
+# def plot_area(pos):
+#     count = new_areas.loc[new_areas.pos == pos]['count']
+#     x, y = m(pos[1], pos[0])
+#     size = (count/1000) ** 2 + 3
+#     m.plot(x, y, 'o', markersize=size, color='#cccccc', alpha=0.8)
+
+#     new_areas.pos.apply(plot_area)
+
+m = Basemap(resolution='l',
+            projection='merc',
+            lat_0=59.922, lon_0=10.736,
+            llcrnrlon=10.65, llcrnrlat=59.887, urcrnrlon=10.8183, urcrnrlat=59.9558)
+
+plot_base(m)
 station_dict = read_stations("test.csv")
-# station_dict = pd.read_csv("test.csv")
+plot_stations(station_dict, m)
+
+plt.savefig('out.png')
+# plt.show()
 
 # Demo dict TODO
-print(station_dict['158'][2])
+# print(station_dict['158'][2])
 
 
-# plot_stations()
-# plot_base()
-# plt.show()
 # def generate_objs(trips_data):
 #     """
 #     Given a trips dataframe, generate minute by minute renders

e708fdd54e861ae1d1c37c6087926c83a45a1b5c
fivism <michael.fivis@gmail.com>
2018-06-25 17:01:39 +0200

    method for pushing to mp4

--- run.sh
+++ run.sh
@@ -6,3 +6,5 @@ python3 controller.py may1.csv
 # call imagemagick on the set of PNGs
 convert -delay 7 -loop 0 -verbose img/*.png animated.gif
 
+# further into h264 mp4 
+# ffmpeg -i animated.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" video.mp4
